60807cf9b8095cd7576ef015400fdc3741e8a81a
Phil Gambling <phil.gambling@experoinc.com>
2019-09-09 09:08:51 -0500

    Enhancements to translate intent

--- lambdas/lex/translate.js
+++ lambdas/lex/translate.js
@@ -1,31 +1,43 @@
 const { translateText } = require('../translate-text');
 
+function initIntentHook(event) {
+  const sessionAttributes =  event.sessionAttributes || {};
+    // hack to pass through free text input for translation
+
+  return {
+      dialogAction: {
+        type: "ElicitSlot",
+        intentName: event.currentIntent.name,
+        slots: {
+            textToTranslate: sessionAttributes.promptedForText ? event.inputTranscript : null
+        },
+        slotToElicit: "textToTranslate",
+      },
+      sessionAttributes: {
+        promptedForText: !sessionAttributes.promptedForText
+      }
+  };
+}
+
 module.exports.handler = async (event) => {
   console.log(JSON.stringify(event));
-  if (event.invocationSource === 'DialogCodeHook') {
-    // TODO: I think need to expect to handle the initial intent prompt as well and return an elicit slot?
-    // hack to pass through free text input for translation
-    return {
-      dialogAction: {
-        type: "Delegate",
-        slots: {
-           textToTranslate: event.inputTranscript,
-        }
-      }
-    };
-  }
-  else {
-    const translatedText = await translateText(event.currentIntent.slots.textToTranslate);
+  if (event.invocationSource === 'DialogCodeHook') return initIntentHook(event);
 
-    return {
-      dialogAction: {
-        type: "Close",
-        fulfillmentState: "Fulfilled",
-        message: {
-          contentType: "PlainText",
-          content: translatedText
-        }
-      }
+  const translatedText = await translateText(event.currentIntent.slots.textToTranslate) || 'Already in English';
+
+  const response = {
+    dialogAction: {
+      type: "Close",
+      fulfillmentState: "Fulfilled"
+    }
+  };
+
+  if (translatedText) {
+    response.dialogAction.message = {
+      contentType: "PlainText",
+      content: translatedText
     };
   }
+
+  return response;
 };
