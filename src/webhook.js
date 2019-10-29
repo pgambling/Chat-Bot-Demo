9528a39b11d458b5f5d5cc7fc213ce17065d2655
Phil Gambling <phil.gambling@experoinc.com>
2019-10-29 10:45:56 -0500

    Added easter egg

--- src/webhook.js
+++ src/webhook.js
@@ -1,4 +1,5 @@
 const AWS = require("aws-sdk");
+const request = require("request-promise-native");
 
 const NO_OP_RESPONSE = { statusCode: 204 };
 
@@ -36,6 +37,30 @@ function message(chatId, text) {
   return {
     statusCode: 200,
     body: JSON.stringify(responseBody)
+  };
+}
+
+async function canariasEasterEgg(chatId, messageId) {
+  const imgUrl = "https://i.imgflip.com/3eo1vd.jpg";
+
+  await request({
+    method: "POST",
+    uri: `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/deleteMessage`,
+    body: {
+      chat_id: chatId,
+      message_id: messageId
+    },
+    json: true
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      chat_id: chatId,
+      method: "sendPhoto",
+      photo: imgUrl,
+      message: imgUrl
+    })
   };
 }
 
@@ -91,17 +116,21 @@ exports.handler = async function(event) {
 
   if (!body.message) return NO_OP_RESPONSE;
 
-  const { from, chat, text } = body.message;
+  const { from, chat, text, message_id } = body.message;
 
   try {
     if (!text) return NO_OP_RESPONSE;
+
+    console.log(`Received "${text}" from ${from.username} in chat ${chat.id}`);
 
     if (text === "/start") {
       console.log(`Sending welcome message to ${from.username}`);
       return welcomeMessage(chat.id, from);
     }
 
-    console.log(`Received "${text}" from ${from.username} in chat ${chat.id}`);
+    if (text === "/easteregg") {
+      return await canariasEasterEgg(chat.id, message_id);
+    }
 
     // concatenating the user and chat ids so Lex can distinguish between seperate conversations
     const senderId = `telegram-${from.id}-${chat.id}`;
@@ -110,9 +139,10 @@ exports.handler = async function(event) {
     const telegramResponse = convertLexReplyToTelegram(chat.id, lexReply);
     console.log("Sending response to telegram");
     console.log(JSON.stringify(telegramResponse));
-    
+
     return telegramResponse;
   } catch (err) {
+    console.log(err);
     // return something in case of error so Telegram doesn't keep sending the same update
     return message(chat.id, "I don't feel right");
   }
